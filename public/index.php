6bf833b2012587b91f38904846f569e991d0f0d6
harlequiin <maris.udris.lv@gmail.com>
2018-01-08 08:11:41 +0200

    Split development vs production databases

--- public/index.php
+++ public/index.php
@@ -4,9 +4,13 @@ require '../vendor/autoload.php';
 
 use Medoo\Medoo;
 
+$file = '../storage/database.db';
+if (is_writable('../storage/database.local.db')) {
+  $file = '../storage/database.local.db';
+}
 $database = new Medoo([
   'database_type' => 'sqlite',
-  'database_file' => '../storage/database.db'
+  'database_file' => $file
 ]);
 
 $comment = new Sitepoint\Comment($database);
